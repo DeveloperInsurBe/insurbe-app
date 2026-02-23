b5a8207b306b88803d252be2f20af7bdfbb93211
dalip03 <dalipdivakar@gmail.com>
2026-02-23 16:12:43 +0530

    Protect insuranceSignupFlow

---
+++ src/app/middleware.ts
@@ -0,0 +1,18 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => {
+      const allowedEmails = [
+        "developer@insurbe.com",
+        "admin@insurbe.com",
+      ];
+
+      return !!token && allowedEmails.includes(token.email as string);
+    },
+  },
+});
+
+export const config = {
+  matcher: ["/insuranceSignupFlow"],
+};
